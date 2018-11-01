030451f71ed4eae2e00f0743ae8d86ba8f14f201
vino5794 <43897232+vino5794@users.noreply.github.com>
2018-11-01 13:58:00 -0400

    new changes

--- Section8/src/Demo_4.java
+++ Section8/src/Demo_4.java
@@ -1,7 +1,7 @@
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.support.ui.Select;;
 
 public class Demo_4 {
 
